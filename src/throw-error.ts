13faa7486b1ee4f63ea221ed1cd8ea89dc87455b
sam bacha <sam@manifoldfinance.com>
2022-07-19 04:20:24 -0700

    feat(reporting): error docs link

---
+++ src/throw-error.ts
@@ -0,0 +1,21 @@
+/** 
+ * @since v1.0.0
+ * @version Errors.1.0.0
+*/
+
+const gitRepo = 'https://github.com/manifoldfinance/eth-rpc-errors'
+
+/*
+ * Throws an error with a code and description.
+ *
+ * @export ThrowError
+ * @param {String} code Error code
+ * @param {String} error Error description
+ * @see {@link https://github.com/manifoldfinance/eth-rpc-errors/blob/master/docs/Errors.md}
+ *
+ */
+
+export default function ThrowError(code, error) {
+  throw `${code}: ${error}\n\nMore details: ${gitRepo}/blob/master/docs/Errors.md#${code.toLowerCase()}`;
+}
+
