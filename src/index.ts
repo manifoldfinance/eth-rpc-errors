59fd96e8fb28aeefb24ad6fdbb7608da0d82db46
s bacha <sam@manifoldfinance.com>
2022-10-03 09:31:16 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+/**
+* 
+* @packageName Eth Rpc Errors
+*/
+
 import { EthereumRpcError, EthereumProviderError } from './classes';
 import {
   serializeError, getMessageFromCode,
 } from './utils';
 import { ethErrors } from './errors';
 import { errorCodes } from './error-constants';
+export * as ErrorInterface from './interface';
 
 export {
   errorCodes,
